3958e71916826264c9f2af7c3551ba8e521cded1
Superdose <72471357+Superdose@users.noreply.github.com>
2023-05-29 20:22:10 +0200

    Update recognition of axis and bars

--- Color_Contrast_28_5.py
+++ Color_Contrast_28_5.py
@@ -146,6 +146,37 @@ def color_long_rectangle(pixel_array, x, y, x_length, y_length, new_color, toler
                 pixel_array[ii, jj] = new_color
 
 
+def color_wide_rectangle(pixel_array, x, y, x_length, y_length, new_color, tolerance_threshold):
+    black = (0, 0, 0)
+    if pixel_array[x, y] != black:
+        sys.exit("Can't color a wide rectangle, if starting pixel is not black")
+
+    start_y = y
+    end_y = y
+
+    start_x = x
+    end_x = x
+
+    while 1:
+        new_end_x = extend_rectangle_to_the_right(pixel_array, end_x, start_y, end_y, x_length, tolerance_threshold)
+        new_start_x = extend_rectangle_to_the_left(pixel_array, start_x, start_y, end_y, tolerance_threshold)
+        new_end_y = extend_rectangle_downwards(pixel_array, new_start_x, new_end_x, end_y, y_length, tolerance_threshold)
+        new_start_y = extend_rectangle_upwards(pixel_array, new_start_x, new_end_x, start_y, tolerance_threshold)
+
+        if new_start_y == start_y and new_end_y == end_y and new_start_x == start_x and new_end_x == end_x:
+            break
+
+        start_y = new_start_y
+        end_y = new_end_y
+        start_x = new_start_x
+        end_x = new_end_x
+
+    for ii in range(start_x, end_x + 1):
+        for jj in range(start_y, end_y + 1):
+            if pixel_array[ii, jj] == black:
+                pixel_array[ii, jj] = new_color
+
+
 def count_connected_pixels_in_row_from(pixel_array, x, y, x_length):
     black = (0, 0, 0)
     if pixel_array[x, y] != black:
@@ -217,9 +248,17 @@ def get_rectangle_type_from(pixel_array, x, y, x_length, y_length):
 
 
 ## returns -> (x, y, rectangle_type)
-def check_rectangle_type_of_first_left_upper_rectangle_and_get_coordinates(pixel_array, x_length, y_length):
+def check_first_left_upper_rectangle_and_get_coordinates(pixel_array, x_length, y_length):
     for ii in range(x_length):
         for jj in range(y_length):
+            if pixel_array[ii, jj] == (0, 0, 0):
+                return ii, jj, get_rectangle_type_from(pixel_array, ii, jj, x_length, y_length)
+
+
+## returns -> (x, y, rectangle_type)
+def check_first_lower_right_rectangle_and_get_coordinates(pixel_array, x_length, y_length):
+    for ii in reversed(range(x_length)):
+        for jj in reversed(range(y_length)):
             if pixel_array[ii, jj] == (0, 0, 0):
                 return ii, jj, get_rectangle_type_from(pixel_array, ii, jj, x_length, y_length)
 
@@ -310,6 +349,55 @@ def delete_islands_without_color_and_recolor_target_island_to_black(pixel_array,
                 pixel_array[ii, jj] = (255, 255, 255)
 
 
+## Mutates filter stage pixels
+def merge_filter_stage_pixels_with_axis_and_bar_recognition_pixels(filter_stage_pixels, axis_bar_recognition_pixels, x_length, y_length):
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+
+    for ii in range(x_length):
+        for jj in range(y_length):
+            px = axis_bar_recognition_pixels[ii, jj]
+            if px != white and px != black:
+                filter_stage_pixels[ii, jj] = px
+
+
+def does_pixel_exist(x, y, x_length, y_length):
+    if x < 0 or y < 0:
+        return 0
+    if x >= x_length or y >= y_length:
+        return 0
+    return 1
+
+
+def remove_pixels_with_color_if_they_have_no_neighbours_in_a_nine_field(pixel_array, x_length, y_length, color):
+    white = (255, 255, 255)
+
+    for ii in range(x_length):
+        for jj in range(y_length):
+            if pixel_array[ii, jj] == color:
+                left_x = ii - 1
+                right_x = ii + 1
+                upper_y = jj - 1
+                lower_y = jj + 1
+
+                if does_pixel_exist(left_x, upper_y, x_length, y_length) and pixel_array[left_x, upper_y] == color:
+                    continue
+                if does_pixel_exist(ii, upper_y, x_length, y_length) and pixel_array[ii, upper_y] == color:
+                    continue
+                if does_pixel_exist(right_x, upper_y, x_length, y_length) and pixel_array[right_x, upper_y] == color:
+                    continue
+                if does_pixel_exist(left_x, jj, x_length, y_length) and pixel_array[left_x, jj] == color:
+                    continue
+                if does_pixel_exist(right_x, jj, x_length, y_length) and pixel_array[right_x, jj] == color:
+                    continue
+                if does_pixel_exist(left_x, lower_y, x_length, y_length) and pixel_array[left_x, lower_y] == color:
+                    continue
+                if does_pixel_exist(ii, lower_y, x_length, y_length) and pixel_array[ii, lower_y] == color:
+                    continue
+                if does_pixel_exist(right_x, lower_y, x_length, y_length) and pixel_array[right_x, lower_y] == color:
+                    continue
+                pixel_array[ii, jj] = white
+
 
 
 # open method used to open different extension image file
@@ -318,6 +406,8 @@ im = Image.open("images/image3.jpg")
 pixelCount = np.full((256, 256, 256), 0, dtype=int)
 
 pixels = im.load()
+
+im.show("Original image")
 
 for i in range(im.size[0]):
     for j in range(im.size[1]):
@@ -343,6 +433,8 @@ contrast_cut = 200
 imageWidth = im.width
 imageHeight = im.height
 
+
+## Filter background
 for i in range(im.size[0]):
     for j in range(im.size[1]):
         red = pixels[i, j][0]
@@ -356,6 +448,13 @@ for i in range(im.size[0]):
         else:
             pixels[i, j] = (255, 255, 255)
 
+im.show("After first filter step - no pixel neighbour translation")
+
+image_filter_stage_copy = im.copy()
+image_filter_stage_pixels = image_filter_stage_copy.load()
+
+
+## Filter pixels, that only have one neighbour with the same color
 for i in range(im.size[0]):
     for j in range(im.size[1]):
 
@@ -367,6 +466,8 @@ for i in range(im.size[0]):
             same_color_count = count_same_color_pixel(pixels, i, j, (255, 255, 255), im.size[0], im.size[1])
             if same_color_count == 1:
                 pixels[i, j] = (0, 0, 0)
+
+im.show("After second filter step - pixel neighbour translation")
 
 most_pixels = 0
 green_counter = 0
@@ -395,9 +496,11 @@ for i in range(im.size[0]):
 
 delete_islands_without_color_and_recolor_target_island_to_black(pixels, im.size[0], im.size[1], color_of_biggest_island)
 
+im.show()
+
 ## first left hit needs to be y axis -> check if long or wide -> if long -> draw (tolerant) rectangle with priority on length
 
-upper_left_rectangle = check_rectangle_type_of_first_left_upper_rectangle_and_get_coordinates(pixels, im.size[0], im.size[1])
+upper_left_rectangle = check_first_left_upper_rectangle_and_get_coordinates(pixels, im.size[0], im.size[1])
 
 if upper_left_rectangle[2] == RectangleType.WIDE:
     sys.exit("The first upper left rectangle supposedly is a wide rectangle. "
@@ -405,10 +508,39 @@ if upper_left_rectangle[2] == RectangleType.WIDE:
 
 color_long_rectangle(pixels, upper_left_rectangle[0], upper_left_rectangle[1], im.size[0], im.size[1], (0, 255, 0), 0.1)
 
-## TODO: als nächstes x-Achse erkennen und danach die einzelnen Balken
+lower_right_rectangle = check_first_lower_right_rectangle_and_get_coordinates(pixels, im.size[0], im.size[1])
+
+if lower_right_rectangle[2] == RectangleType.LONG:
+    sys.exit("The first lower right rectangle supposedly is a long rectangle. "
+             "This shouldn't be the case, as this should be the x-axis, which should be a wide rectangle")
+
+color_wide_rectangle(pixels, lower_right_rectangle[0], lower_right_rectangle[1], im.size[0], im.size[1], (0, 0, 255), 0.1)
+
+
+
+
+for i in range(im.size[0]):
+    for j in range(im.size[1]):
+        if pixels[i, j] == (0, 0, 0):
+            rect = (i, j, get_rectangle_type_from(pixels, i, j, im.size[0], im.size[1]))
+            rect_x = rect[0]
+            rect_y = rect[1]
+            rec_type = rect[2]
+            if rec_type == RectangleType.WIDE:
+                color_wide_rectangle(pixels, rect_x, rect_y, im.size[0], im.size[1], (255, 0, 0), 0.1)
+            else:
+                color_long_rectangle(pixels, rect_x, rect_y, im.size[0], im.size[1], (255, 0, 0), 0.1)
 
 
 im.show()
 
-## TODO: Continue here -> eventuell 2 Klassen -> erste Repräsentiert Pixelfarbe (z.B. einfach Enum Weiß/schwarz oder so) -> zweite Repräsentiert ganzes Bild
-## Eventuell senkst du die Rechenleistung, wenn du statt auf einem richtigen Bild auf einer einfache Abstraktion arbeitest
+## Merge with original filter (that still has text in it)
+merge_filter_stage_pixels_with_axis_and_bar_recognition_pixels(image_filter_stage_pixels, pixels, im.size[0], im.size[1])
+
+remove_pixels_with_color_if_they_have_no_neighbours_in_a_nine_field(image_filter_stage_pixels, im.size[0], im.size[1], (0, 0, 0))
+
+image_filter_stage_copy.show()
+
+## Read Text
+
+##
